9a0c45584f6bde4e83ed673a66c06cfdc7fa271a
Hans Engel <engel@engel.uk.to>
2011-10-13 17:06:21 -0700

    Added header, header_body, footer views

--- lwt-include/template.php
+++ lwt-include/template.php
@@ -46,86 +46,28 @@ Developed by J. Pierre in 2011.
 <?php
 }
 
-function pagestart_nobody($titeltext, $addcss='') {
+function pagestart_nobody($page_title, $extra_css = '') {
 	global $debug;
 	@header( 'Expires: Wed, 11 Jan 1984 05:00:00 GMT' );
 	@header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' );
 	@header( 'Cache-Control: no-cache, must-revalidate, max-age=0' );
 	@header( 'Pragma: no-cache' );
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
 
-<!-- ***********************************************************
-"Learning with Texts" (LWT) is released into the Public Domain.
-This applies worldwide.
-In case this is not legally possible, any entity is granted the
-right to use this work for any purpose, without any conditions,
-unless such conditions are required by law.
-
-Developed by J. Pierre in 2011.
-************************************************************ -->
-
-<head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-
-	<meta name="viewport" content="width=900" />
-	<link rel="apple-touch-icon" href="img/apple-touch-icon-57x57.png" />
-	<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png" />
-	<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png" />
-	<link rel="apple-touch-startup-image" href="img/apple-touch-startup.png">
-	<meta name="apple-mobile-web-app-capable" content="yes" />
-
-	<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
-	<link rel="stylesheet" type="text/css" href="css/jquery.tagit.css">
-	<link rel="stylesheet" type="text/css" href="css/tagit.ui-zendesk.css">
-	<link rel="stylesheet" type="text/css" href="css/styles.css">
-	<style type="text/css">
-	<?php echo $addcss . "\n"; ?>
-	</style>
-
-	<script type="text/javascript" src="js/jquery.js" charset="utf-8"></script>
-	<script type="text/javascript" src="js/jquery.scrollTo.min.js" charset="utf-8"></script>
-	<script type="text/javascript" src="js/jquery-ui.min.js"  charset="utf-8"></script>
-	<script type="text/javascript" src="js/tag-it.js" charset="utf-8"></script>
-	<script type="text/javascript" src="js/sorttable/sorttable.js" charset="utf-8"></script>
-	<script type="text/javascript" src="js/countuptimer.js" charset="utf-8"></script>
-	<script type="text/javascript" src="js/overlib/overlib_mini.js" charset="utf-8"></script>
-	<script type="text/javascript">
-	//<![CDATA[
-	<?php echo "var STATUSES = " . json_encode(get_statuses()) . ";\n"; ?>
-	<?php echo "var TAGS = " . json_encode(get_tags()) . ";\n"; ?>
-	<?php echo "var TEXTTAGS = " . json_encode(get_texttags()) . ";\n"; ?>
-	//]]>
-	</script>
-	<script type="text/javascript" src="js/pgm.js" charset="utf-8"></script>
-	<script type="text/javascript" src="js/jq_pgm.js" charset="utf-8"></script>
-
-	<title>Learning with Texts :: <?php echo $titeltext; ?></title>
-</head>
-<body>
-<div id="overDiv" style="position:absolute; visibility:hidden; z-index:1000;"></div>
-<?php
+  render('header');
 }
 
-function pagestart($titeltext,$close) {
+function pagestart($page_title, $close) {
 	global $debug;
-	pagestart_nobody($titeltext);
-	echo '<h4>';
-	if ($close) echo '<a href="index.php" target="_top">';
-	echo '<img class="lwtlogo" src="img/lwt_icon.png" alt="Logo" />Learning with Texts';
-	if ($close) {
-		echo '</a>&nbsp; | &nbsp;';
-		quickMenu();
-	}
-	echo '</h4><h3>' . $titeltext . ($debug ? ' <span class="red">DEBUG</span>' : '') . '</h3>';
-	echo "<p>&nbsp;</p>";
+
+	pagestart_nobody($page_title);
+  render('header_body');
 }
 
 function pageend() {
 	global $debug;
 	if ($debug) showRequest();
-?></body></html><?php
+
+  render('footer');
 }
 
 function quickMenu() {
---
+++ lwt-view/header_body.php
@@ -0,0 +1,8 @@
+<h4>
+    <?php if ( $close ): ?><a href="index.php" target="_top"><?php endif; ?>
+    <img class="lwtlogo" src="img/lwt_icon.png" alt="Logo" /> Learning with Texts
+    <?php if ( $close ): ?></a>&nbsp; | &nbsp;<?php quickMenu(); endif; ?>
+</h4>
+
+<h3><?php echo $page_title . ( $debug ? ' <span class="red">DEBUG</span>' : '' ); ?></h3>
+<p>&nbsp;</p>
