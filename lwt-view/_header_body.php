98e248e5902e1f03b922900a02e9901a039469ad
Hans Engel <engel@engel.uk.to>
2011-10-16 09:52:53 -0700

    Added _header_body template

---
+++ lwt-view/_header_body.php
@@ -0,0 +1,17 @@
+<?php
+?><h4>
+<?php if ( $close ) { ?>
+
+<a href="index.php" target="_top">
+<img alt="Logo" class="lwtlogo" src="img/lwt_icon.png" /><?php echo "&nbsp;"; ?>Learning with Texts
+</a><?php } else { ?>
+
+<img alt="Logo" class="lwtlogo" src="img/lwt_icon.png" />Learning with Texts
+<?php } ?>
+</h4><h3>
+<?php echo $page_title . ( $debug ? ' <span class="red">DEBUG</span>' : '' ); ?>
+
+</h3><p>
+<?php echo '&nbsp;'; ?>
+
+</p>
