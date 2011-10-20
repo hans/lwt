d8422ce4f84aabe1f3703bc770dded0d916584f9
Hans Engel <engel@engel.uk.to>
2011-10-19 17:45:06 -0700

    Removed leftover PHP template file

--- lwt-view/header_body.php
+++
@@ -1,8 +0,0 @@
-<h4>
-    <?php if ( $close ): ?><a href="index.php" target="_top"><?php endif; ?>
-    <img class="lwtlogo" src="img/lwt_icon.png" alt="Logo" /> Learning with Texts
-    <?php if ( $close ): ?></a>&nbsp; | &nbsp;<?php quickMenu(); endif; ?>
-</h4>
-
-<h3><?php echo $page_title . ( $debug ? ' <span class="red">DEBUG</span>' : '' ); ?></h3>
-<p>&nbsp;</p>
