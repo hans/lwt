dc32aaade8f97a70440c625b61b96b5fda896586
LWT Project <lwt-project@hotmail.com>
2013-06-13 21:48:06 +0200

    Version 1.5.3 (13-Jun-2013 21:47)

--- inline_edit.php
+++ inline_edit.php
@@ -20,12 +20,12 @@ include "connect.inc.php";
 include "utilities.inc.php";
 
 $value = (isset($_POST['value'])) ? $_POST['value'] : "";
+$value = trim($value);
 $id = (isset($_POST['id'])) ? $_POST['id'] : "";
 
 if (substr($id, 0, 5) == "trans") {
-	// usleep(200000);
 	$id = substr($id, 5);
-	if(trim($value) == '') $value='*';
+	if($value == '') $value='*';
 	$message = runsql('update ' . $tbpref . 'words set WoTranslation = ' . 
 		convert_string_to_sqlsyntax(repl_tab_nl($value)) . ' where WoID = ' . $id,
 		"");
@@ -34,12 +34,16 @@ if (substr($id, 0, 5) == "trans") {
 }
 
 if (substr($id, 0, 5) == "roman") {
-	// usleep(100000);
+	if ($value == '*') $value='';
 	$id = substr($id, 5);
 	$message = runsql('update ' . $tbpref . 'words set WoRomanization = ' . 
 		convert_string_to_sqlsyntax(repl_tab_nl($value)) . ' where WoID = ' . $id,
 		"");
-	echo get_first_value("select WoRomanization as value from " . $tbpref . "words where WoID = " . $id);
+	$value = get_first_value("select WoRomanization as value from " . $tbpref . "words where WoID = " . $id);
+	if ($value == '') 
+		echo '*';
+	else 
+		echo $value;
 	exit;
 }
 
