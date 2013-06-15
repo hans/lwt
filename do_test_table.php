3b0ccbd1386dd49d22d39b982f1635b6cd3b41b2
LWT Project <lwt-project@hotmail.com>
2013-06-15 15:07:23 +0200

    Version 1.5.4 (15-Jun-2013 15:06)

---
+++ do_test_table.php
@@ -0,0 +1,66 @@
+<?php
+
+/**************************************************************
+"Learning with Texts" (LWT) is released into the Public Domain.
+This applies worldwide.
+In case this is not legally possible, any entity is granted the
+right to use this work for any purpose, without any conditions, 
+unless such conditions are required by law.
+
+Developed by J. P. in 2011, 2012, 2013.
+***************************************************************/
+
+/**************************************************************
+Call: do_test_table.php?lang=[langid]
+Call: do_test_test.php?text=[textid]
+Call: do_test_test.php?&selection=1  
+			(SQL via $_SESSION['testsql'])
+Show test frame with vocab table
+***************************************************************/
+
+include "settings.inc.php";
+include "connect.inc.php";
+include "utilities.inc.php";
+
+$p = '';
+
+if (isset($_REQUEST['selection']) && isset($_SESSION['testsql'])) {
+	$testsql = $_SESSION['testsql']; 
+}
+
+elseif (isset($_REQUEST['lang'])) {
+	$testsql = ' ' . $tbpref . 'words where WoLgID = ' . $_REQUEST['lang'] . ' '; 
+}
+
+elseif (isset($_REQUEST['text'])) {
+	$testsql = ' ' . $tbpref . 'words, ' . $tbpref . 'textitems where TiLgID = WoLgID and TiTextLC = WoTextLC and TiTxID = ' . $_REQUEST['text'] . ' ';
+}
+
+else die("Called with wrong parameters");
+
+pagestart_nobody('','');
+
+$cntlang = get_first_value('select count(distinct WoLgID) as value from ' . $testsql);
+if ($cntlang > 1) {
+	echo '<p>Sorry - The selected terms are in ' . $cntlang . ' languages, but tests are only possible in one language at a time.</p>';
+	pageend();
+	exit();
+}
+
+?>
+<div id="body">
+<?php
+
+$count = get_first_value('SELECT count(distinct WoID) as value FROM ' . $testsql);
+echo '<p>COUNT: ' . $count . '</p>';
+echo '<p>NOT YET IMPLEMENTED</p>';
+
+?>
+
+</div>
+
+<?php
+
+pageend();
+
+?>
---
+++ start.php
@@ -0,0 +1,77 @@
+<?php
+
+/**************************************************************
+"Learning with Texts" (LWT) is released into the Public Domain.
+This applies worldwide.
+In case this is not legally possible, any entity is granted the
+right to use this work for any purpose, without any conditions, 
+unless such conditions are required by law.
+
+Developed by J. P. in 2011, 2012, 2013.
+***************************************************************/
+
+/**************************************************************
+Call: start.php
+Analyse DB tables, select Table Set, start LWT
+***************************************************************/
+
+include "settings.inc.php";
+include "connect.inc.php";
+include "utilities.inc.php";
+
+if ($fixed_tbpref) {
+	header("Location: index.php");
+	exit(); 
+}
+
+if (isset($_REQUEST['prefix'])) {
+	if($_REQUEST['prefix'] !== '-') {
+		$tbpref = $_REQUEST['prefix'];
+		LWTTableSet ("current_table_prefix", $tbpref);
+		header("Location: index.php");
+		exit(); 
+	}
+}
+
+$prefix = getprefixes();
+
+if (count($prefix) == 0) {
+	$tbpref = '';
+	LWTTableSet ("current_table_prefix", $tbpref);
+	header("Location: index.php");
+	exit(); 
+}
+
+pagestart('Select Table Set',false);
+
+?>
+
+<table class="tab1" style="width: auto;" cellspacing="0" cellpadding="5">
+
+<tr>
+<th class="th1">
+<form name="f1" class="inline" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+<p>Select: <select name="prefix">
+<option value="" <?php echo ($tbpref == '' ? 'selected="selected"': ''); ?>>Default Table Set</option>
+<?php
+foreach ($prefix as $value) {
+?>
+<option value="<?php echo tohtml($value); ?>" <?php echo (substr($tbpref,0,-1) == $value ? 'selected="selected"': ''); ?>><?php echo tohtml($value); ?></option>
+<?php
+}
+?>
+</select> 
+</p>
+<p class="center"><input type="submit" name="op" value="Start LWT" />
+</p>
+</form>
+</th>
+</tr>
+
+</table>
+
+<?php
+
+pageend();
+
+?>
