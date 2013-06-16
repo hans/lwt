d9688cebcb4e9d1cd03a2f9e53d613abbd578bdb
LWT Project <lwt-project@hotmail.com>
2013-06-16 15:30:14 +0200

    Version 1.5.4 (16-Jun-2013 15:30)

--- do_test_table.php
+++ do_test_table.php
@@ -61,15 +61,15 @@ $regexword = get_first_value('select LgRegexpWordCharacters as value from ' . $t
 $(function() {
 $('#cbTerm').click(function() {
 	if($('#cbTerm').is(':checked')) 
-		$('td:nth-child(1)').css('color', 'black').css('cursor', 'auto');
+		$('td:nth-child(2)').css('color', 'black').css('cursor', 'auto');
 	else 
-		$('td:nth-child(1)').css('color', 'white').css('cursor', 'pointer');
+		$('td:nth-child(2)').css('color', 'white').css('cursor', 'pointer');
 });
 $('#cbRom').click(function() {
 	if($('#cbRom').is(':checked')) 
-		$('td:nth-child(2),th:nth-child(2)').show();
+		$('td:nth-child(4),th:nth-child(4)').show();
 	else 
-		$('td:nth-child(2),th:nth-child(2)').hide();
+		$('td:nth-child(4),th:nth-child(4)').hide();
 });
 $('#cbTrans').click(function() {
 	if($('#cbTrans').is(':checked')) 
@@ -79,45 +79,44 @@ $('#cbTrans').click(function() {
 });
 $('#cbSentence').click(function() {
 	if($('#cbSentence').is(':checked'))
-		$('td:nth-child(4),th:nth-child(4)').show();
-	else 
-		$('td:nth-child(4),th:nth-child(4)').hide();
-});
-$('#cbStatus').click(function() {
-	if($('#cbStatus').is(':checked')) 
 		$('td:nth-child(5),th:nth-child(5)').show();
 	else 
 		$('td:nth-child(5),th:nth-child(5)').hide();
 });
+$('#cbStatus').click(function() {
+	if($('#cbStatus').is(':checked')) 
+		$('td:nth-child(1),th:nth-child(1)').show();
+	else 
+		$('td:nth-child(1),th:nth-child(1)').hide();
+});
 $('td').click(function() {
 	$(this).css('color', 'black').css('cursor', 'auto');
 });
-$('td:nth-child(2),th:nth-child(2)').hide();
-$('td:nth-child(4),th:nth-child(4)').hide();
 $('td:nth-child(5),th:nth-child(5)').hide();
+$('td:nth-child(4),th:nth-child(4)').hide();
 $('td').css('background-color', 'white');
 });
 //]]>
 </script>
 <p>
+<input type="checkbox" id="cbStatus" checked="checked" /> Status
 <input type="checkbox" id="cbTerm" checked="checked" /> Term
-<input type="checkbox" id="cbRom" /> Romanization
 <input type="checkbox" id="cbTrans" checked="checked" /> Translation
+<input type="checkbox" id="cbRom" /> Romanization
 <input type="checkbox" id="cbSentence" /> Sentence
-<input type="checkbox" id="cbStatus" /> Status
 </p>
 
 <table class="sortable tab1" style="width:auto;" cellspacing="0" cellpadding="5">
 <tr>
-<th class="th1 clickable">Term</th>
-<th class="th1 clickable">Romanization</th>
-<th class="th1 clickable">Translation</th>
-<th class="th1 clickable">Sentences</th>
 <th class="th1 clickable">Status</th>
+<th class="th1 clickable">Term</th>
+<th class="th1 clickable">Translation</th>
+<th class="th1 clickable">Romanization</th>
+<th class="th1 clickable">Sentences</th>
 </tr>
 <?php
 
-$sql = 'SELECT DISTINCT WoID, WoText, WoTranslation, WoRomanization, WoSentence, (ifnull(WoSentence,\'\') not like concat(\'%{\',WoText,\'}%\')) as notvalid, WoStatus, WoTodayScore As Score FROM ' . $testsql . ' AND WoStatus BETWEEN 1 AND 5 AND WoTranslation != \'\' AND WoTranslation != \'*\' order by WoTodayScore, WoRandom';
+$sql = 'SELECT DISTINCT WoID, WoText, WoTranslation, WoRomanization, WoSentence, WoStatus, WoTodayScore As Score FROM ' . $testsql . ' AND WoStatus BETWEEN 1 AND 5 AND WoTranslation != \'\' AND WoTranslation != \'*\' order by WoTodayScore, WoRandom';
 if ($debug) echo $sql;
 $res = mysql_query($sql);		
 if ($res == FALSE) die("Invalid Query: $sql");
@@ -127,16 +126,24 @@ while ($record = mysql_fetch_assoc($res)) {
 		mask_term_in_sentence($sent,$regexword)));
 	$score = $record['Score'];
 	if ( $score < 0 ) 
-		$score = ' <img src="icn/status-busy.png" title="Test today!" alt="Test today!" />';
+		$scoret = '<span class="red2">' . $record['WoStatus'] . '</span>';
 	else
-		$score = ' <img src="icn/status.png" title="-" alt="-" />';
+		$scoret = $record['WoStatus'];
+	if ( $record['WoStatus'] < 5 ) 
+		$plus = '<img src="icn/plus.png" title="+" alt="+" />';
+	else
+		$plus = '<img src="icn/placeholder.png" title="" alt="" />';
+	if ( $record['WoStatus'] > 1 ) 
+		$minus = '<img src="icn/minus.png" title="-" alt="-" />';
+	else
+		$minus = '<img src="icn/placeholder.png" title="" alt="" />';
 ?>
 <tr>
+<td class="td1 center"><?php echo $minus . ' ' . $scoret . ' ' . $plus; ?></td>
 <td class="td1 center"><?php echo $span1 . tohtml($record['WoText']) . $span2; ?></td>
-<td class="td1 center"><?php echo tohtml($record['WoRomanization']); ?></td>
 <td class="td1 center"><?php echo tohtml($record['WoTranslation']); ?></td>
+<td class="td1 center"><?php echo tohtml($record['WoRomanization']); ?></td>
 <td class="td1 center"><?php echo $span1 . $sent1 . $span2; ?></td>
-<td class="td1 center"><?php echo tohtml($record['WoStatus']) . $score; ?></td>
 </tr>
 <?php
 }
