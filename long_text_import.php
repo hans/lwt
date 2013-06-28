bacb290ef840a2e49bfac7aa17d6b2ecc5982dd5
LWT Project <lwt-project@hotmail.com>
2013-06-28 18:00:01 +0200

    Version 1.5.9 (28-Jun-2013 17:59)

---
+++ long_text_import.php
@@ -0,0 +1,33 @@
+<?php
+
+/**************************************************************
+"Learning with Texts" (LWT) is released into the Public Domain.
+This applies worldwide.
+In case this is not legally possible, any entity is granted the
+right to use this work for any purpose, without any conditions, 
+unless such conditions are required by law.
+
+Developed by J. P. in 2011, 2012, 2013.
+***************************************************************/
+
+/**************************************************************
+Call: long_text_import.php?...
+			op=...
+Long Text Import
+***************************************************************/
+
+include "settings.inc.php";
+include "connect.inc.php";
+include "utilities.inc.php";
+
+pagestart('Long Text Import',true);
+
+?>
+
+<p> Not yet implemented.</p>
+
+<?php
+
+pageend();
+
+?>
