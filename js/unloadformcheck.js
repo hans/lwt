83ccb79bc3e0fb7f41adbdec5db201b585069f42
LWT Project <lwt-project@hotmail.com>
2013-07-16 10:40:23 +0200

    Version 1.5.12 (16-Jul-2013 10:40)

--- js/unloadformcheck.js
+++ js/unloadformcheck.js
@@ -28,21 +28,15 @@ function resetDirty() {
 	DIRTY = 0; 
 }
 
-function bindForChange(){    
-	$('input,checkbox,textarea,radio,select').bind('change',makeDirty);
-	$(':reset,:submit').bind('click',resetDirty);
-}
-
 function tagChanged(event, ui) {
-	if (! ui.duringInitialization) makeDirty();
+	if (! ui.duringInitialization) DIRTY = 1;
 	return true;
 }
-
-window.onbeforeunload = askConfirmIfDirty;
-window.onload = bindForChange;
 
 $(document).ready( function() {
 	$('#termtags').tagit({afterTagAdded: tagChanged, afterTagRemoved: tagChanged});
 	$('#texttags').tagit({afterTagAdded: tagChanged, afterTagRemoved: tagChanged}); 
+	$('input,checkbox,textarea,radio,select').bind('change',makeDirty);
+	$(':reset,:submit').bind('click',resetDirty);
+	$(window).bind('beforeunload', askConfirmIfDirty);
 } ); 
-
