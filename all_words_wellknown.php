72d3df6f9bc94da10429dfd4ff4b3705eec17c1f
HugoFara <hugo.farajallah@protonmail.com>
2022-04-16 16:06:05 +0900

    Finished the all_words_wellknown refactoring.

--- all_words_wellknown.php
+++ all_words_wellknown.php
@@ -9,7 +9,6 @@
  *       all_words_wellknown.php?text=[textid]&status=[statusint] 
  *                              (mark with a specific status, normally 98 or 99)
  * 
- * 
  * @package Lwt
  * @author  LWT Project <lwt-project@hotmail.com>
  * @license Unlicense <http://unlicense.org/>
@@ -19,22 +18,37 @@
 
 require_once 'inc/session_utility.php';
 
+/**
+ * Make the SQL query for all words in the text.
+ * 
+ * @param int $txid Text id
+ * 
+ * @return mysqli_result|true SQL query.
+ */
 function all_words_wellknown_get_words($txid)
 {
     global $tbpref;
-    $sql = 'SELECT DISTINCT Ti2Text, LOWER(Ti2Text) AS Ti2TextLC
-    FROM (' . 
-        $tbpref . 'textitems2 
-        LEFT JOIN ' . $tbpref . 'words 
+    $sql = "SELECT DISTINCT Ti2Text, LOWER(Ti2Text) AS Ti2TextLC
+    FROM ( 
+        {$tbpref}textitems2 
+        LEFT JOIN {$tbpref}words 
         ON LOWER(Ti2Text) = WoTextLC AND Ti2LgID = WoLgID
     ) 
-    WHERE WoID IS NULL AND Ti2WordCount = 1 AND Ti2TxID = ' . $txid . ' 
-    ORDER BY Ti2Order';
+    WHERE WoID IS NULL AND Ti2WordCount = 1 AND Ti2TxID = $txid 
+    ORDER BY Ti2Order";
     return do_mysqli_query($sql);
 }
 
-// $sqlarr = null;
-
+/**
+ * For each word, add the word to the database.
+ * 
+ * @param int    $status New word status
+ * @param string $term   Word to mark
+ * @param string $termlc Same as $term, but in lowercase.
+ * @param int    $langid Language ID
+ * 
+ * @return array<int, string> Number of rows edited and a javascript query.
+ */
 function all_words_wellknown_process_word($status, $term, $termlc, $langid)
 {
     global $tbpref;
@@ -47,32 +61,45 @@ function all_words_wellknown_process_word($status, $term, $termlc, $langid)
             $langid . ', ' . 
             convert_string_to_sqlsyntax($term) . ', ' . 
             convert_string_to_sqlsyntax($termlc) . ', ' . $status . ' , NOW(), ' .  
-            make_score_random_insert_update('id') 
-        . ')', 
+            make_score_random_insert_update('id') .
+        ')', 
         ''
     );
     if (!is_numeric($message)) {
         my_die('ERROR: Could not modify words! Message: ' + $message);
     }
     if ((int)$message == 0) {
-        error_message_with_hide('WARNING: No rows modified! Message: ' + $message, false);
+        error_message_with_hide(
+            'WARNING: No rows modified! Message: ' + $message, 
+            false
+        );
     }
     $rows = (int) $message;
     $wid = get_last_key();
-    // $sqlarr[]= ' WHEN ' . convert_string_to_sqlsyntax_notrim_nonull($termlc) . ' THEN ' . $wid;
     $javascript = '';
     if (getSettingWithDefault('set-tooltip-mode') == 1 && $rows > 0) {
-        $javascript .= "title = make_tooltip(" . prepare_textdata_js($term) . ",'*','','".$status."');";
+        $javascript .= "title = make_tooltip(" . 
+        prepare_textdata_js($term) . ", '*', '', '" . $status . 
+        "');";
     }
     $javascript .= "$('.TERM" . strToClassName($termlc) . "', context)
     .removeClass('status0')
-    .addClass('status".$status." word" . $wid . "')
-    .attr('data_status', '".$status."')
+    .addClass('status" . $status . " word" . $wid . "')
+    .attr('data_status', '" . $status . "')
     .attr('data_wid', '" . $wid . "')
     .attr('title', title);";
     return array($rows, $javascript);
 }
 
+/**
+ * Main processing loop to mark all words of a text with a new status.
+ * 
+ * @param int $txid   Text ID
+ * @param int $status New status to apply to all words.
+ * 
+ * @return array<int,string> Number of edited words, 
+ *                           and JavaScript query to change their display  
+ */
 function all_words_wellknown_main_loop($txid, $status)
 {
     global $tbpref;
@@ -95,6 +122,14 @@ function all_words_wellknown_main_loop($txid, $status)
     return array($count, $javascript);
 }
 
+/**
+ * Display the number of edited words.
+ * 
+ * @param int $status New status
+ * @param int $count  Number of edited words. 
+ * 
+ * @return void
+ */
 function all_words_wellknown_count_terms($status, $count)
 {
     if ($status == 98) {
@@ -104,27 +139,52 @@ function all_words_wellknown_count_terms($status, $count)
     }
 }
 
-function all_words_wellknown_javascript($javascript)
+/**
+ * Execute JavaScript to change the display of all words.
+ * 
+ * @param int $txid       Text ID
+ * @param int $javascript JavaScript-formatted string.
+ * 
+ * @return void
+ */
+function all_words_wellknown_javascript($txid, $javascript)
 {
     ?>
 <script type="text/javascript">
     //<![CDATA[
     const context = window.parent.document;
     <?php echo $javascript; ?> 
-    $('#learnstatus', context).html('<?php echo addslashes(texttodocount2($_REQUEST['text'])); ?>');
+    $('#learnstatus', context)
+    .html('<?php echo addslashes(texttodocount2($txid)); ?>');
     window.parent.setTimeout('cClick()', 1000);
     //]]>
 </script>
     <?php
 }
 
+/**
+ * Make the main content of the page for all well-known words.
+ * 
+ * @param int $txid   Text ID
+ * @param int $status New status to apply to words.
+ * 
+ * @return void
+ */
 function all_words_wellknown_content($txid, $status)
 {
     list($count, $javascript) = all_words_wellknown_main_loop($txid, $status);
     all_words_wellknown_count_terms($status, $count);
-    all_words_wellknown_javascript($javascript);
+    all_words_wellknown_javascript($txid, $javascript);
 }
 
+/**
+ * Make a full HTML page for all well-known words.
+ * 
+ * @param int $txid   Text ID
+ * @param int $status New status to apply to words.
+ * 
+ * @return void
+ */
 function all_words_wellknown_full($txid, $status) 
 {
     if ($status == 98) {
